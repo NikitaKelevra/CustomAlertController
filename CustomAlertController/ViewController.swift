721a355911c9619fb7857df531a89a2736db855f
Никита <81251222+NikitaKelevra@users.noreply.github.com>
2022-02-12 11:49:19 +0300

    Финальная версия по коду, но без картинок IMAGE  к сожалению

--- CustomAlertController/ViewController.swift
+++ CustomAlertController/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let myButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 20
+        button.backgroundColor = .purple
+        button.setTitle("Show Alert", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
 
+    let customAlert = MyAlert()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue.withAlphaComponent(60)
+        setConstrains()
+        
+        myButton.addAction(UIAction { [weak self] _ in
+            self?.myButtonTapped()}, for: .touchUpInside)
     }
 
+    
+    func myButtonTapped() {
+        customAlert.showAlert(title: "LIKE", message: "SWIFT Greg", viewVontroller: self)
+    }
 
 }
 
+
+
+
+
+
+
+extension ViewController {
+    func setConstrains() {
+        view.addSubview(myButton)
+        NSLayoutConstraint.activate([
+            myButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            myButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            myButton.heightAnchor.constraint(equalToConstant: 70),
+            myButton.widthAnchor.constraint(equalToConstant: 300)
+
+        
+        ])
+    }
+}
